06b161bc9369beef554b5cd30361891eaebf244f
FrancineHuang <huangxin130@gmail.com>
2024-01-03 15:50:49 +0900

    learned about the fragment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-    return (<h2>My First Component</h2>);
+    return (
+        <>
+            <div>
+                <h3>Hello World</h3>
+                <ul>
+                    <li>
+                        <a href="#">Hello list 1</a>
+                    </li>
+                </ul>
+            </div>
+            <h2>Hello World</h2>
+        </>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

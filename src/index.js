096b023a5bec48fecac69600d0071d9a38b75d48
FrancineHuang <huangxin130@gmail.com>
2024-01-05 08:42:02 +0900

    fix: refactor the code

--- src/index.js
+++ src/index.js
@@ -14,28 +14,18 @@ function BookList() {
 }
 
 const Book = () => {
+    const author = 'Jordan Moore';
+    const title = 'The Heaven & Earth Grocery Store: A Novel';
     return (
         <article className='book'>
-            <Image />
-            <Title />
-            <Author />
+            <img
+                src='https://m.media-amazon.com/images/I/411-+bcHfKL._SY445_SX342_.jpg'
+                alt='The Heaven & Earth Grocery Store: A Novel'
+            />
+            <h2>{title}</h2>
+            <h4>{author}</h4>
         </article>
     )
-}
-
-const Image = () => (
-    <img 
-        src='https://m.media-amazon.com/images/I/411-+bcHfKL._SY445_SX342_.jpg'
-        alt='The Heaven & Earth Grocery Store: A Novel'
-    />
-)
-
-const Title = () => {
-    return <h2>The Heaven & Earth Grocery Store: A Novel</h2>
-}
-
-const Author = () => {
-    return <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop:'0.5rem'}}>James McBride</h4>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

676c06a658219a575ff098f248a89c5e6a0a6689
FrancineHuang <huangxin130@gmail.com>
2024-01-10 07:47:25 +0900

    fix: refactor the code

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+    {
+        author: 'Jordan Moore',
+        title: 'Interesting Facts For Curious Minds',
+        image: 'https://m.media-amazon.com/images/I/411-+bcHfKL._SY445_SX342_.jpg',
+        id: 1,
+    },
+    {
+        author: 'James Clear',
+        title: 'Atomic Habits',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+        id: 2,
+    }
+];
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
+import { books } from './books'
 
-const books = [
-    {
-        author: 'Jordan Moore',
-        title: 'Interesting Facts For Curious Minds',
-        image: 'https://m.media-amazon.com/images/I/411-+bcHfKL._SY445_SX342_.jpg',
-    },
-    {
-        author: 'James Clear',
-        title: 'Atomic Habits',
-        image: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-    }
-];
 
 function BookList() {
     return (
